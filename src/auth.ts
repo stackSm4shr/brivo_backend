00bbaf59de2df3d9305f2573e8b4ec912d6f11ae
Tim <timbecker1988@gmail.com>
2026-03-26 09:04:44 +0100

    fix cross domain

--- src/auth.ts
+++ src/auth.ts
@@ -8,17 +8,22 @@ const { db } = await connectToDatabase();
 export const auth = betterAuth({
   secret: process.env.BETTER_AUTH_SECRET,
   baseURL: process.env.BETTER_AUTH_URL,
-
   trustedOrigins: [
-  process.env.CORS_ORIGIN,
-  "http://localhost:3000",
-].filter((origin): origin is string => Boolean(origin)),
-
+    process.env.CORS_ORIGIN,
+    "http://localhost:3000",
+  ].filter((origin): origin is string => Boolean(origin)),
   emailAndPassword: {
     enabled: true,
   },
-
   database: mongodbAdapter(db, { client }),
+  advanced: {
+    useSecureCookies: true,
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      partitioned: true,
+    },
+  },
 });
 
 export type Session = typeof auth.$Infer.Session;
